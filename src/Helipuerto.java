9f0330b087c8d0e046250f0e5618be57dfa2e753
DAM1 <DAM1@PC301>
2014-05-26 09:53:58 +0200

    asd

---
+++ src/Helipuerto.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author DAM1
+ */
+public class Helipuerto {
+    
+    void añadir(Volador v){
+        
+        
+    }
+    
+    boolean eliminar(int numero){
+        
+        return true;
+    }
+    
+    String aterrizar(int puerto, int metros){
+        return "";
+    }
+    
+    String despegar(int helicoptero){
+        
+        return "";
+    }
+    
+}
